3c9208f230d21aaf1deadba8ed3e11b342c55823
Davide Barranca <undavide@gmail.com>
2014-06-26 18:13:09 +0200

    Added Factories to Angular, removed main.js and themeManager.js

--- src/com.undavide.tools2/js/app.js
+++ src/com.undavide.tools2/js/app.js
@@ -1,17 +1,125 @@
 var app = angular.module('ToolsApp', []);
 
-app.controller('ToolsController', function ($scope, $http) {
+app.factory('CSI', function() {
+	
+	var csInterface = new CSInterface();
 
+	// Convert the Color object to string in hexadecimal format; 
+	var toHex = function (color, delta) {
+
+		function computeValue(value, delta) {
+			var computedValue = !isNaN(delta) ? value + delta : value;
+			if (computedValue < 0) {
+				computedValue = 0;
+			} else if (computedValue > 255) {
+				computedValue = 255;
+			}
+
+			computedValue = Math.floor(computedValue);
+
+			computedValue = computedValue.toString(16);
+			return computedValue.length === 1 ? "0" + computedValue : computedValue;
+		}
+
+		var hex = "";
+		if (color) {
+			hex = computeValue(color.red, delta) + computeValue(color.green, delta) + computeValue(color.blue, delta);
+		}
+		return hex;
+	}
+
+	// Add a rule in a stylesheet
+	var addRule = function (stylesheetId, selector, rule) {
+		var stylesheet = document.getElementById(stylesheetId);
+		if (stylesheet) {
+			stylesheet = stylesheet.sheet;
+			if (stylesheet.addRule) {
+				stylesheet.addRule(selector, rule);
+			} else if (stylesheet.insertRule) {
+				stylesheet.insertRule(selector + ' { ' + rule + ' }', stylesheet.cssRules.length);
+			}
+		}
+	}
+	
+	// Update the theme with the appropriate Topcoat CSS using appSkinInfo
+	// to determine which one to load - then overrides some Topcoat properties
+	var updateThemeWithAppSkinInfo = function (appSkinInfo) {
+
+		// console.log(JSON.stringify(appSkinInfo));
+
+		var themeShade = "",
+			redShade = appSkinInfo.panelBackgroundColor.color.red;
+
+		if (redShade > 200) { // exact: 214 (#D6D6D6)
+			themeShade = "lightlight"; // might be useful in the future
+			// this is where font color and other theme dependent stuff could go
+			$("#topcoat").attr("href", "css/topcoat-desktop-lightlight.min.css");
+		
+		} else if (redShade > 180) { // exact: 184 (#B8B8B8)
+			themeShade = "light";
+			$("#topcoat").attr("href", "css/topcoat-desktop-light.min.css");
+
+		} else if (redShade > 80) { // exact: 83 (#535353)
+			themeShade = "dark";
+			$("#topcoat").attr("href", "css/topcoat-desktop-dark.min.css");
+		
+		} else if (redShade > 50) { // exact: 52 (#343434)
+			themeShade = "darkdark";
+			$("#topcoat").attr("href", "css/topcoat-desktop-darkdark.min.css");
+		}
+
+		var styleId = "topcoat-host",
+			fontColor = themeShade.match(/light/) ? "#202020" : "#E6E6E6"
+			
+		addRule(styleId, "body", "font-family:" + appSkinInfo.baseFontFamily );
+		addRule(styleId, "body", "font-size:" + appSkinInfo.baseFontSize + "px");
+		addRule(styleId, "body", "color:" + fontColor);
+
+		// Set the Tools icons stylesheet and Font color
+		if (redShade > 128) { // light theme
+			$("#icons").attr("href", "css/icons-light-theme.css");	
+		} else { // dark theme
+			$("#icons").attr("href", "css/icons-dark-theme.css");	
+		}
+	}
+
+	// Callback for the CSInterface.THEME_COLOR_CHANGED_EVENT
+	var onAppThemeColorChanged = function (event) {
+		var skinInfo = JSON.parse(window.__adobe_cep__.getHostEnvironment()).appSkinInfo;
+		updateThemeWithAppSkinInfo(skinInfo);
+	}
+	
+	var syncTheme = function () {
+		updateThemeWithAppSkinInfo(csInterface.hostEnvironment.appSkinInfo);
+		csInterface.addEventListener(CSInterface.THEME_COLOR_CHANGED_EVENT, onAppThemeColorChanged);		
+	}
+
+	return {
+		csInterface: csInterface,
+		syncTheme: syncTheme
+	}
+});
+
+app.factory('StringIDs', ['$http', function($http) {
+
+	return {
+		getObject: function() { return $http.get('./js/tools.json') }
+	}
+	
+}]);
+
+app.controller('ToolsController', ['$scope', '$http', 'CSI', 'StringIDs', function ($scope, $http, CSI, StringIDs) {
+
+	CSI.syncTheme();
 	$scope.psToolsModel = null;
-	// currentTool =  null;
-	$scope.csInterface = new CSInterface();
+	$scope.csInterface = CSI.csInterface;
 
 	// Get the StringID of the currently selected Tool
 	// so that when the panel opens it already has the right Tool selected
-	$scope.csInterface.evalScript('getCurrentTool()', function (stringID) { // callback
+	CSI.csInterface.evalScript('getCurrentTool()', function (stringID) { // callback
 
 		// Fetch the local JSON (array of Tools objects)
-		$http.get('./js/tools.json')
+		StringIDs.getObject()
 
 			.success( function (data) {
 				// fill the psToolsModel to be used by ng-repeat directive
@@ -31,9 +139,9 @@ app.controller('ToolsController', function ($scope, $http) {
 				console.log("Error reading the tools file!");
 			});
 	});
-});
+}]);
 
-app.directive('psTools', function () {
+app.directive('psTools', [ 'CSI', function (CSI) {
 	return {
 		restrict: 'E',
 		replace: true,
@@ -47,8 +155,8 @@ app.directive('psTools', function () {
 			}
 			// calls the selection function in JSX
 			$scope.selectTool = function() { 
-				$scope.csInterface.evalScript("selectTool('" + $scope.type + "')");
+				CSI.csInterface.evalScript("selectTool('" + $scope.type + "')");
 			};
 		}
 	}
-});
+}]);
